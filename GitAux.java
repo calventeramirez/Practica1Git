634472573ca3a62d4ca30403d0d056626f89fcd3
calventeramirez <calventeramirez@outlook.com>
2015-03-06 17:10:29 +0100

    Version inicial el proyecto

---
+++ GitAux.java
@@ -0,0 +1,30 @@
+package pkgGitHub;
+
+/**
+ *
+ * @author alumno: Pablo Jesús Calvente Ramírez
+ * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <escribe tu nombre aqui>
+ * 
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+	 System.out.println("modificado por alum 1: Pablo Jesús Calvente Ramírez");
+    }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+    }
+    
+    public void metodoAlumno3(){
+        System.out.println("metodo 3");
+    }
+    
+    public void metodoComunitario(){
+        System.out.println("Aqui escribimos todos");
+	// modificado por el alumno 1:Pablo Jesus Calvente Ramirez
+	 System.out.println("alumno 1: Pablo Jesus Calvente Ramirez inserta esta sentencia");
+    }
+}
