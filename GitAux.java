5eeb0204ca6374a5037f51c5603f8e5b2bc6bbde
calventeramirez <calventeramirez@outlook.com>
2015-03-06 18:03:02 +0100

    Volvemos a la situacion de inicio(modificacion alumno1)

--- GitAux.java
+++ GitAux.java
@@ -3,7 +3,7 @@ package pkgGitHub;
 /**
  *
  * @author alumno: <Pablo Jesus Calvente Ramirez>
- * @author alumno: <Eduardo Serralvo Gil>
+ * @author alumno: <escribe tu nombre aqui>
  * @author alumno: <escribe tu nombre aqui>
  * 
  *
@@ -15,11 +15,12 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+	System.out.println("modificado por Pablo");//alumno1
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-	System.out.println("modificado por alumno 2"); // alumno 2
+	
     }
     
     public void metodoAlumno3(){
@@ -28,7 +29,7 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-	// modificaciones alumno 2
-	System.out.println("Alumno 2 inserta esta sentencia");
+	// modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
     }
 }
