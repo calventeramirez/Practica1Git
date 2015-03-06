254344531016d560b28495a19afc23eeee9c40b5
MLRivero <miguelinares.11@gmail.com>
2015-03-06 17:55:32 +0100

    modificaciones alumno 3

--- GitAux.java
+++ GitAux.java
@@ -4,7 +4,7 @@ package pkgGitHub;
  *
  * @author alumno: <Pablo>
  * @author alumno: <escribe tu nombre aqui>
- * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <Miguel Linares Rivero>
  * 
  *
 *
@@ -23,9 +23,12 @@ public class GitAux {
     
     public void metodoAlumno3(){
         System.out.println("metodo 3");
+	System.out.println("modificado por Miguel Linares"); // alumno 3
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 3
+	System.out.println("Alumno 3 inserta esta sentencia");	
     }
 }
